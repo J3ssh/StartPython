60dfa83127b2582c4e1244d8f257c0cabcda7075
J3ssh <87490958+J3ssh@users.noreply.github.com>
2021-09-12 14:32:27 +0100

    Create 001.py
    Challenge 001 from Python by example by Nichola Lacey

---
+++ PythonByExample/The Basics 1-11/001.py
@@ -0,0 +1,15 @@
+#My Soloution
+kilogram = int(input("Enter KG amount: "))
+convert = kilogram / 0.45359237
+print ("The Answer is", convert)
+
+
+'''
+Answer in book
+
+kilo = init(input("Enter the number of kilos: "))
+pound = kilo * 2.204
+print ("That is", pound, "pounds")
+
+
+'''
