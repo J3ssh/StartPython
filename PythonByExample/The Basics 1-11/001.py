461aa464765dc9f344d76a486f815f2badcdf255
J3ssh <87490958+J3ssh@users.noreply.github.com>
2021-09-12 14:33:14 +0100

    Update 001.py
    Challenge 001 from Python by example by Nichola Lacey

--- PythonByExample/The Basics 1-11/001.py
+++ PythonByExample/The Basics 1-11/001.py
@@ -1,15 +1,10 @@
 #My Soloution
-kilogram = int(input("Enter KG amount: "))
-convert = kilogram / 0.45359237
-print ("The Answer is", convert)
-
-
+nameAsk = input("Hi Whats your name?")
+print ("Hello", nameAsk)
 '''
-Answer in book
+Answers in book
 
-kilo = init(input("Enter the number of kilos: "))
-pound = kilo * 2.204
-print ("That is", pound, "pounds")
-
+firstname = input ("Please enter your first name: ")
+print ("Hello", firstname)
 
 '''
