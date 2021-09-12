793b19f4631f3e6bbdd3cb15ded5f4daf80fec2a
J3ssh <87490958+J3ssh@users.noreply.github.com>
2021-09-12 14:34:49 +0100

    Create 003.py
    Challenge 003 from Python by example by Nichola Lacey
    
    Part of the challenge was to only use online to force me to use \n for  a newline in print

---
+++ PythonByExample/The Basics 1-11/003.py
@@ -0,0 +1,10 @@
+#My Solution
+print ("What do you call a bear with no teeth? \n A gummy bear haha")
+
+'''
+Answer in book
+
+print ("What do you call a bear with no teeh?\nA gummy bear!")
+
+
+'''
