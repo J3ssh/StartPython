8659fd6cc9fa6e997edd85586ca75ffdfa68dbf5
J3ssh <87490958+J3ssh@users.noreply.github.com>
2021-09-12 14:41:25 +0100

    Create 006.py
    Challenge 006 from Python by example by Nichola Lacey

---
+++ PythonByExample/The Basics 1-11/006.py
@@ -0,0 +1,16 @@
+#My Solution
+startPizza = int(input("How many silces did you start with?: "))
+slicesAte = int(input("How many Slices did you eat?: "))
+slicesLeft = startPizza - slicesAte
+print("There are this many slices left", slicesLeft)
+
+
+'''
+Answer in book
+
+startNum = int(input("Please enter your first number: "))
+endNum = int(input("How many slices have you eaten? "))
+slicesLeft = startNum -endNum
+print ("You have", slicesLeft, "Slices remaning")
+
+'''
