87baac2eca3643dda45fcc79f6c1c02e4b41584f
J3ssh <87490958+J3ssh@users.noreply.github.com>
2021-09-12 14:33:39 +0100

    Create 002.py
    Challenge 002 from Python by example by Nichola Lacey

---
+++ PythonByExample/The Basics 1-11/002.py
@@ -0,0 +1,14 @@
+#My Solution
+firstName = input("Whats your first name?")
+lastName = input("Whats your last name?")
+print ("Hello", firstName, lastName)
+
+'''
+Answer in book
+
+firstname = input("Please enter your first name: ") 
+surname = input("Please enter your surname: ")
+print ("Hello", firstname, surname)
+
+
+'''
