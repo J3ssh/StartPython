455bce3bdf4b11568cab6862d6c1062f96adaf70
J3ssh <87490958+J3ssh@users.noreply.github.com>
2021-09-12 14:43:06 +0100

    Create 008.py
    Challenge 008 from Python by example by Nichola Lacey

---
+++ PythonByExample/The Basics 1-11/008.py
@@ -0,0 +1,17 @@
+#My Solution
+totalPrice = int(input("whats the total bill price?: "))
+dinners = int(input("how many dinners are there?: "))
+equalPay = totalPrice / dinners
+print ("Everyone must pay", equalPay)
+
+
+'''
+Answer in book
+
+bill = int(input("What is the total cost of the bill? "))
+people = int(input("How many people are there? "))
+each = bill/people
+print ("Each persons should pay £", each)
+
+
+'''
