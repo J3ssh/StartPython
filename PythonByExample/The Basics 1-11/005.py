7d7803c969b61731a123ce2f1f280bffe8880fae
J3ssh <87490958+J3ssh@users.noreply.github.com>
2021-09-12 14:36:17 +0100

    Create 005.py
    Challenge 005 from Python by example by Nichola Lacey

---
+++ PythonByExample/The Basics 1-11/005.py
@@ -0,0 +1,21 @@
+#My Solution
+num1 = int(input("Enter first Number to add"))
+num2 = int(input("Enter second Number to add"))
+num3 = int(input("Enter number to multiply"))
+Answer = num1 + num2
+multi = Answer * num3
+print ("The total is", multi)
+
+
+'''
+Answer in book
+
+num1 = int(input("Please enter your first number: "))
+num2 = int(input("Please enter your second number: "))
+num3 = int(input("Please enter your third number: "))
+answer = (num1 + num2) * num3
+print ("The answer is", answer)
+
+
+
+'''
