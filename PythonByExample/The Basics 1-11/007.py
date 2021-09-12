7fcc172a1abbbe87facdccd23987c11e5e1c1b34
J3ssh <87490958+J3ssh@users.noreply.github.com>
2021-09-12 14:42:22 +0100

    Create 007.py
    Challenge 007 from Python by example by Nichola Lacey

---
+++ PythonByExample/The Basics 1-11/007.py
@@ -0,0 +1,17 @@
+#My Solution
+askName = input("Whats your name?:")
+askAge = int(input("Whats your age?:"))
+newAge = askAge + 1
+print (askName, "next birthday you will be", newAge)
+
+
+'''
+Answer in book
+
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+newAge = age + 1
+print (name, "next birthday you will be ", newAge)
+
+
+'''
