b7f1259df16591551913be4869f00c91a00aec6b
J3ssh <87490958+J3ssh@users.noreply.github.com>
2021-09-12 14:35:35 +0100

    Create 004.py
    Challenge 004 from Python by example by Nichola Lacey

---
+++ PythonByExample/The Basics 1-11/004.py
@@ -0,0 +1,15 @@
+#My Solution
+numb1 = int(input("Enter number 1"))
+numb2 = int(input("Enter number 2"))
+print ("The total is", numb1 + numb2)
+
+'''
+Answer in book
+
+num1 = int(input("Please enter your first number: "))
+num2 = int(input("Please enter your second number: "))
+answer = num1 + num2
+print ("The Answer is", answer)
+
+
+'''
