d42dbe0192a5eb1137efdc63ee269d8d6e49152e
J3ssh <87490958+J3ssh@users.noreply.github.com>
2021-09-12 14:43:40 +0100

    Create 009.py
    Challenge 009 from Python by example by Nichola Lacey

---
+++ PythonByExample/The Basics 1-11/009.py
@@ -0,0 +1,26 @@
+#My Solution
+days = int(input("Enter the amount of days: "))
+hours = days * 24
+minutes = days * 1440
+seconds = days * 86400
+print("Days to hours", hours)
+print("Days to minutes", minutes)
+print("Days to seconds", seconds)
+
+'''
+Answer in Book
+
+days = int(input("Enter the number of days: "))
+hours = daus*24
+minutes = hours*60
+seconds = minutes*60
+print ("in", days, "days there are....")
+print (hours, "hours")
+print (minutes, "minutes")
+print (seconds, "seconds")
+
+
+
+
+
+'''
