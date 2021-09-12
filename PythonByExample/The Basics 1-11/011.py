f5a8ca6be706da157e1918fe25360106d8a3b8b7
J3ssh <87490958+J3ssh@users.noreply.github.com>
2021-09-12 14:45:07 +0100

    Create 011.py
    Challenge 011 from Python by example by Nichola Lacey

---
+++ PythonByExample/The Basics 1-11/011.py
@@ -0,0 +1,17 @@
+#My Solution
+over100 = int(input("Enter a number over 100: "))
+under10 = int(input("Enter a number under 10: "))
+answer = over100 / under10
+
+print ("The answer is", answer)
+
+
+'''
+Answer in the book
+
+larger = init(input("Enter a number over 100: "))
+smaller = int(input("Enter a number a under 10: "))
+answer = larger//smaller
+print (smaller, "goes into", larger, answer, "times")
+
+'''
