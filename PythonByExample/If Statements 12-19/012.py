9224626903417067276de162ebc72c3dcc8f02a8
J3ssh <87490958+J3ssh@users.noreply.github.com>
2021-09-15 10:17:35 +0100

    Create 012.py

---
+++ PythonByExample/If Statements 12-19/012.py
@@ -0,0 +1,18 @@
+num1 = int(input("Please enter first number: "))
+num2 = int(input("Pleas enter second number: "))
+if num1 > num2:
+    print (num2, num1)
+else:
+    print (num1, num2)
+
+
+    ''' Answer in book
+    
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+    if num1 > num2:
+        print (num2, num1)
+  else:
+    print: (num1, num2)
+    
+    '''
